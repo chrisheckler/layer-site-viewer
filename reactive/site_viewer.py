af812e2723a61771aaf85a8e1295a04ae0c81fe7
Chris Heckler <hecklerchris@hotmail.com>
2018-08-06 11:33:02 -0700

    Makefile, tox.ini added. Charm is good to go

--- reactive/site_viewer.py
+++ reactive/site_viewer.py
@@ -14,10 +14,20 @@ from charmhelpers.core.hookenv import (
     status_set,
     log,
     config,
+    open_port,
 )
 
-FLASK_DIR = Path('/srv/site-view-app')
-FLASK_DEPS = Path(FLASK_DIR / 'requirements.txt')
+from charmhelpers.core.host import (
+    service_running,
+    service_start,
+    service_restart,
+)
+
+from charmhelpers.core.templating import render
+
+
+FLASK_DIR = Path('/srv/site_view_app')
+FLASK_DEPS = FLASK_DIR / 'requirements.txt'
 PIP = Path('/usr/bin/pip3')
 
 
@@ -28,13 +38,39 @@ def install_flask_deps():
     conf = config()
     status_set('maintenance', 'Flask/Dependencies Installing')
 
+    if FLASK_DIR.exists():
+        call(['rm', '-rf', str(FLASK_DIR)])
     call(['git', 'clone', conf.get('git-flask-repo'), str(FLASK_DIR)])
     call([str(PIP), 'install', '-r', str(FLASK_DEPS)])
     call([str(PIP), 'install', 'gunicorn'])
 
-    status_set('active', 'Flask and Dependencies Installed')
+    status_set('active', 'Flask/Dependencies Installed')
     log('Flask and Dependencies Installed')
     set_flag('app.flask.installed')
+
+
+@when('app.flask.installed')
+@when_not('app.flask.running')
+def flask_systemd_started():
+    """Configure systemd to run application"""
+
+    status_set('maintenance', 'Configuring systemd')
+
+    render('flask.service.tmpl', '/etc/systemd/system/flaskapp.service',
+           context={})
+    call(['systemctl', 'flaskapp'])
+    call(['systemctl', 'enable', 'flaskapp'])
+
+    if not service_running('flaskapp'):
+        service_start('flaskapp')
+    else:
+        service_restart('flaskapp')
+
+    open_port(5000)
+
+    status_set('active', 'Flask App Running')
+    log('Flask App Running')
+    set_flag('app.flask.running')
 
 
 
